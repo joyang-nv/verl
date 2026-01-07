bf2ba9566aed419f5cf7235ed8e80b268faf7ca8
Liwei Ma <liweim@nvidia.com>
2026-01-07 10:37:37 +0800

    fix nnodes

--- examples/grpo_trainer/run_qwen2-7b_math_trtllm.sh
+++ examples/grpo_trainer/run_qwen2-7b_math_trtllm.sh
@@ -12,6 +12,7 @@ export RAY_DEDUP_LOGS=0
 # Config
 # -----
 TP=${1:-4}
+NNODES=${NNODES:-1}
 PROJECT_NAME=${PROJECT_NAME:-"verl_grpo_example_gsm8k_math"}
 EXP_NAME=trtllm-qwen2-7b-tp${TP}-8gpus${EXP_NAME_SUFFIX:+"-"}${EXP_NAME_SUFFIX}
 
@@ -81,7 +82,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name="${PROJECT_NAME}" \
     trainer.experiment_name=${EXP_NAME} \
     trainer.n_gpus_per_node=8 \
-    trainer.nnodes=1 \
+    trainer.nnodes=$NNODES \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
     trainer.resume_mode=disable \
